456ced924e9db46790ee6a8499f85363c624e121
scarletsky <scarletsky1025@gmail.com>
2018-01-19 10:58:57 +0800

    feat: add missing props

--- engine/graphics/device.d.ts
+++ engine/graphics/device.d.ts
@@ -45,6 +45,8 @@ declare namespace pc {
          */
         readonly maxVolumeSize: number;
 
+        maxPixelRatio: number;
+
         /**
          * @function
          * @name pc.GraphicsDevice#setViewport
